f9fca69fb1559da7dd2e3e89a3ce019146be3112
Computer vision engineer <112948275+computervisioneng@users.noreply.github.com>
2023-08-05 10:14:22 -0300

    Create util.py

---
+++ util.py
@@ -0,0 +1,24 @@
+import random
+import string
+import json
+
+
+def _from_rgb(rgb):
+    return "#%02x%02x%02x" % rgb
+
+
+def generate_random_string(N=20):
+    return ''.join(random.choice(string.ascii_lowercase + string.ascii_uppercase) for i in range(N))
+
+
+def get_custom_prompts():
+    with open('./custom_prompts.json', 'r') as f:
+        custom_prompts_ = json.load(f)
+        f.close()
+    return custom_prompts_
+
+
+def write_custom_prompts(dict_):
+    with open('./custom_prompts.json', 'w') as f:
+        json.dumps(f, dict_)
+        f.close()
